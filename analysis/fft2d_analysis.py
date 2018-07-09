01adbf74af1fab2cb9eb2d131bfee4921b246b19
Downie <downie4@basswood.llnl.gov>
2018-07-09 14:33:28 -0700

    Created shell script to run bot fft and xcor analysis on specified data directory.

--- analysis/fft2d_analysis.py
+++ analysis/fft2d_analysis.py
@@ -38,11 +38,16 @@ def read_directory(dir_path,ext,datatype):
         print("An error occured, check correct directory was passed.")    
 
     files.sort()
-
+    count = 0.0
+    max = len(files)
     #Create list of numpy arrays containing data for each file
     all_data = []
     for file in files:
         all_data.append(read_data(file,datatype))
+        count += 1
+        progress = (count / max) * 100.0
+        sys.stdout.write('\r[{}] {}%'.format('#'*int(progress/5), round(progress,2)))
+        sys.stdout.flush()
     return all_data
 
 
@@ -159,7 +164,7 @@ def get_stats(timestep,threshold,fft_data,amps,d_freqs):
         aspect = amp/wave
         stats.append([timestep,dom,x,y,amp,phase,wave,aspect])
     
-    return pd.DataFrame(stats,columns=['Time','Dominant Freq.','X','Y','Amplitude','Phase','Wavelength','Aspect_Ratio'])
+    return pd.DataFrame(stats,columns=['Time','Dominant Freq.','X','Y','Amplitude','Phase','Wavelength','Amp/Wave'])
 
 #Returns one large dataframe containing all the results taken from fft analysis of all files in directory
 #threshold -> the threshold value of data to track when providing the stats
@@ -179,7 +184,7 @@ def get_all_stats(threshold, d_freqs, all_fft_data, all_amps):
     
 #Graphs desired data at specific intervals and saves the graph as a png to specified directory, creates a GIF of all images and saves with specified name
 #Returns the total number of images produced
-#int -> the plot interval, e.g. 10 means every 10th element in the data list will be graphed and saved as png
+#int -> the plot interval, e.g. 10 means every 10th element in the data list will be graphed and saved as png, 0 means no graphing
 #dir -> the directory to save the graphs to
 #GIF_name -> the full file name (directory + name + .gif) to use when creating the GIF animation
 #basename -> the base filename to use, each name will be given a index number to make it unique, e.g "filename{}" -> filename0.png, filename1.png... etc.
@@ -188,11 +193,20 @@ def get_all_stats(threshold, d_freqs, all_fft_data, all_amps):
 #fig_size -> (optional) size of the figure to plot
 #x_label,y_label,z_label -> (optional) labels to use for x,y and z-axis
 #title -> (optional) the title to use for plot, note if you have a title like: 'title a {id}' the {id} will be replaced by the index of that data snapshot
-def graph_all(int,dir,GIF_name,basename,data,graph_type,fig_size,x_label,y_label,z_label,title):
+def graph_all(interval,dir,GIF_name,basename,data,graph_type,fig_size,x_label,y_label,z_label,title):
 
+    if interval == 0:
+        return 0
+
+    count = 0.0
+    max = len(data)/interval
     images = []
     for i, d in enumerate(data):
-        if i % int == 0:
+        if i % interval == 0:
+            count += 1
+            progress = count / max * 100
+            sys.stdout.write('\r[{}] {}%'.format('#'*int(progress/5), round(progress,2)))
+            sys.stdout.flush()
             fig = plot_data(d,graph_type,fig_size,x_label,y_label,z_label,title.format(id=i))
             fname = dir + basename.format(i)
             save_to_png(fig,fname)
@@ -274,12 +288,16 @@ def fft2d_analysis(time_step, threshold, data, graph, both):
 
     return stats
 
-def main(directory="input_data/ALT_DATA1/", image_interval=100):
+#directory -> The directory that holds the log files to analyze
+#image_interval -> A graph will be made and saved at every interval. E.g 50 = every 50th data file will be graphed.
+#Note: if image interval is set to 0, no graphs are made.
+def main(directory="input_data/ALT_DATA1/",output_dir="results_1",image_interval=100):
 
     MAIN_DATA_DIR = directory
-    PNG_OUTPUT_DIR = "output_files/results_1/fft/png_output/"
-    CSV_OUTPUT = "output_files/results_1/fft/fft_analysis.csv"
-    GIF_OUTPUT = "output_files/results_1/fft/fft_results.gif"
+    PNG_OUTPUT_DIR = output_dir + "png_output/"
+    DATA_OUTPUT_DIR = output_dir
+    CSV_OUTPUT_NAME = "fft_analysis.csv"
+    GIF_OUTPUT_NAME = "fft_results.gif"
     GRAPH_TYPE = 'surf' #Options available to use, 'surf'->surface, 'wire'->wireframe, 'scat'->scatter, 'cont'->contour
     XLABEL = 'x'
     YLABEL = 'y'
@@ -292,15 +310,19 @@ def main(directory="input_data/ALT_DATA1/", image_interval=100):
     THRESHOLD = 0.8
     DATA_TYPE = int
 
-    #Check all directories exists
-    directories = [MAIN_DATA_DIR, PNG_OUTPUT_DIR, CSV_OUTPUT, GIF_OUTPUT]
+    #Check parent directories exists
+    directories = [MAIN_DATA_DIR, DATA_OUTPUT_DIR]
     for d in directories:
         if not os.path.exists(d):
             print("The specified path: {} was not found. Analysis cancelled.".format(d))
             return 1
 
+    #Make png output directory if needed
+    if image_interval > 0 and not os.path.exists(PNG_OUTPUT_DIR):
+        os.makedirs(PNG_OUTPUT_DIR)
+
     #Get all data from the main directory
-    print("Reading files...")
+    print("Reading data files...")
     t0 = t.time()
     all_data = read_directory(MAIN_DATA_DIR,BASE_EXT,DATA_TYPE)
     t1 = t.time()
@@ -311,7 +333,7 @@ def main(directory="input_data/ALT_DATA1/", image_interval=100):
         print("No files with the extension: '{}' were found. Analysis cancelled.".format(BASE_EXT))
         return 1
     t_read = t1-t0
-    print("Total files read: {} (with ext: '{}')\nRead time: {}s\nCalculating fft2d data on all files...".format(file_count,BASE_EXT,t_read))
+    print("\rTotal files read: {} (with ext: '{}')\nRead time: {}s\nCalculating fft2d data on all files...".format(file_count,BASE_EXT,t_read))
     
     #Get all fft2d data for calculating
     t0 = t.time()
@@ -337,23 +359,27 @@ def main(directory="input_data/ALT_DATA1/", image_interval=100):
     #Concatenate and save data results
     t0 = t.time()
     all_stats = get_all_stats(THRESHOLD,d_freqs,all_fft2d,all_amps)
-    all_stats.to_csv(CSV_OUTPUT)
+    all_stats.to_csv(DATA_OUTPUT_DIR + CSV_OUTPUT_NAME)
     t1 = t.time()
     t_stats = t1 - t0
     print("Analysis results complete time: {}s\nPlotting data at intervals of {} and creating PNG images...".format(t_stats,SNAPSHOT_INTERVAL))
 
     #Graph resulting data at specific intervals and save as images to directory, create GIF of pngs
     t0 = t.time()
-    im_count = graph_all(SNAPSHOT_INTERVAL,PNG_OUTPUT_DIR,GIF_OUTPUT,BASE_FILE_NAME,all_amps,'surf',FIG_SIZE,XLABEL,YLABEL,ZLABEL,TITLE)
+    im_count = graph_all(SNAPSHOT_INTERVAL,PNG_OUTPUT_DIR,DATA_OUTPUT_DIR+GIF_OUTPUT_NAME,BASE_FILE_NAME,all_amps,'surf',FIG_SIZE,XLABEL,YLABEL,ZLABEL,TITLE)
     t1 = t.time()
     t_plot = t1-t0
     t_total = t_read + t_fft2d + t_amps + t_freqs + t_stats + t_plot
-    print("{} PNG's created in: {}s.\nGIF animation complete.\nAnalysis process complete!\nTotal time: {}s".format(im_count,t_plot,t_total))
-
+    if im_count > 0:
+        print("\r{} PNG's created in: {}s.\nGIF animation complete.\nAnalysis process complete!\nTotal time: {}s".format(im_count,t_plot,t_total))
+    else:
+        print("No PNG images or GIF animation made.\nAnalysis process complete!\nTotal time: {}s".format(t_total))
 args = sys.argv
 
-if len(args) > 2:
-    main(args[1],int(args[2]))
+if len(args) > 3:
+    main(args[1],args[2],int(args[3]))
+elif len(args) > 2:
+    main(args[1],args[2])
 elif len(args) > 1:
     main(args[1])
 else:
