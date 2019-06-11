3040a7f16ea3b2137b80a24a5b93b3b8ca68ddcb
Kelly Kochanski <kochanski1@llnl.gov>
2019-06-11 14:55:39 -0700

    Added parallel test - tested test on quartz

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -3,14 +3,14 @@
 # Set up this run in the correct directory
 #Configured for LL CPU not RC
 
-output_root="../new_runs3"
-echo ${output_root}
+output_root="../test_parallel_runs"
+echo "Looking for jobs to run under ${output_root}"
 
 output_dirs=(${output_root}/*)
 
 output_dir="${output_dirs[${PMI_RANK}]}"
 cd ${output_dir}
-echo "Process ${PMI_RANK} in  ${output_dir}"
+echo "Process ${PMI_RANK} running in  ${output_dir}"
 
 # Need to be able to read and execute this directory
 chmod u+rwx *
