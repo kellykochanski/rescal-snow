40b6a600870313e84c5e204cb972856991b4bfce
Richard Barnes <rbarnes@umn.edu>
2019-07-25 16:59:51 -0700

    Fix indentation in recolor.py

--- docs/example_images/snowfall/recolor.py
+++ docs/example_images/snowfall/recolor.py
@@ -13,13 +13,13 @@ import os
 
 dirname = '..'
 for filename in os.listdir(dirname):
-	if filename.startswith('ALTI') and filename.endswith('0_t0.log'):
-			data = np.loadtxt(os.path.join(dirname, filename))
-			vmx = 20
-			if vmx < data.max():
-				print("Warning: some values saturated")
-			plt.imshow(data, cmap='magma', vmin=2, vmax=vmx)
-			plt.colorbar()
-			plt.savefig(filename[:-4]+'_recolored.png')
-			print('Recolored ' + filename)
-			plt.close()
+  if filename.startswith('ALTI') and filename.endswith('0_t0.log'):
+    data = np.loadtxt(os.path.join(dirname, filename))
+    vmx = 20
+    if vmx < data.max():
+      print("Warning: some values saturated")
+    plt.imshow(data, cmap='magma', vmin=2, vmax=vmx)
+    plt.colorbar()
+    plt.savefig(filename[:-4]+'_recolored.png')
+    print('Recolored ' + filename)
+    plt.close()
