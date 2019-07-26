b1892ed5aadb60b0120fd4e414a1af1c94fe1911
Richard Barnes <rbarnes@umn.edu>
2019-07-26 11:55:42 -0700

    Fix 64-bit printing

--- src/doublets.c
+++ src/doublets.c
@@ -25,6 +25,7 @@
  */
 
 
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
@@ -207,7 +208,7 @@ void elimine_doublet(int32_t type, int32_t index, int32_t dir)
         int32_t x,y,z;
         Calcule_xyz(index,x,y,z);
         ErrPrintf("x=%d   y=%d   z=%d\n",x,y,z);
-        ErrPrintf("iter=%llu\n", iter);
+        ErrPrintf("iter=%" PRIu64 "\n", iter);
         exit(-1);
       }
       // on bouche le 'trou' avec le dernier doublet du tableau
